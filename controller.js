c76fea34991067bae91a905915698e56832f2016
Adam <aalston9@gmail.com>
2019-11-25 18:54:49 -0500

    async functions

--- controller.js
+++ controller.js
@@ -11,6 +11,26 @@ function addButtonListeners() {
     $("#loginButton").on('click', () => {
         // add an async method to authenticate user
     });
+    $("#summarizeButton").on('click', () => {
+        // add an async method to get the summary
+    });
+}
+
+async function postCredentials() {
+    let newName = $("#newName").val();
+    let newPass = $("#newPass").val();
+
+    const postcred = await axios ({
+        method: "post",
+        url: "http://localhost:3000/account/create",
+        body: {
+            "name": newName,
+            "pass": newPass,
+            "data": {
+
+            },
+        },
+    });
 }
 
 $(document).ready(() => {

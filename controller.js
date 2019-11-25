8554a2eed02a09ab4ad75c236fc598b68b4bc11a
eriflo <ericf97@aol.com>
2019-11-25 17:54:54 -0500

    button listeners

--- controller.js
+++ controller.js
@@ -1,6 +1,21 @@
+// import axios from "node_modules/axios/dist/axios.min";
 //api key 9720744B0C
 /**
  * How to make an API request to SMMRY
  * 
  */
+function addButtonListeners() {
+    $("#signupButton").on('click', () => {
+        // add an async method to create account
+    });
+    $("#loginButton").on('click', () => {
+        // add an async method to authenticate user
+    });
+}
+
+$(document).ready(() => {
+    addButtonListeners();
+});
+
+
 

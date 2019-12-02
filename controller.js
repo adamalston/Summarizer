1f56194fc3caf2e66c3f6899a5b04815168a0f91
Adam <aalston9@gmail.com>
2019-12-02 18:27:29 -0500

    finish signup and login async methods

--- controller.js
+++ controller.js
@@ -4,9 +4,11 @@
  * How to make an API request to SMMRY
  * 
  */
+
+
 function hideElements() {
-    $(".accountError").hide();
-    $(".signinError").hide();
+    $(".signupError").hide();
+    $(".loginError").hide();
 }
 const smmryURL = axios.create({
     baseURL: "https://api.smmry.com/SM_API_KEY=9720744B0C"
@@ -20,7 +22,6 @@ function addButtonListeners() {
     $("#signupButton").on('click', () => {
         hideElements();
         postCredentials();
-
     });
     $("#loginButton").on('click', () => {
         hideElements();
@@ -34,24 +35,29 @@ function addButtonListeners() {
 async function postCredentials() {
     try {
         const result = await accountRoot.post(`/create`,{
-        "name": $("#newName").val(),
-        "pass": $("#newPass").val(),
-    });
+            "name": $("#newName").val(),
+            "pass": $("#newPass").val(),
+            "data": {
+                "firstname": $("#firstname").val(),
+                "lastname": $("#lastname").val(),
+            }
+        });
+
         window.location.replace("login.html");
     } catch (error) {
-        $(".accountError").show();
+        $(".signupError").show();
     } 
 }
 
 async function postLogin() {
     try {
         const result = await accountRoot.post(`/login`,{
-        "name": $("#userName").val(),
+        "name": $("#name").val(),
         "pass": $("#pass").val(),
     });
         window.location.replace("index.html");
     } catch (error) {
-        $(".signinError").show();
+        $(".loginError").show();
     }
 }
 

1438ea226c447417afec29c2c7612ee2aac93e16
eriflo <ericf97@aol.com>
2019-12-11 13:44:25 -0500

    Loading SMMRY objects

--- controller.js
+++ controller.js
@@ -1,13 +1,12 @@
-// import axios from "node_modules/axios/dist/axios.min";
 // api key 9720744B0C
 /**
  * How to make an API request to SMMRY
  */
 
-function hideElements() {
-    $(".signupError").hide();
-    $(".loginError").hide();
-    $(".summarizeError").hide();
+function hideElements() {
+    $(".signupError").hide();
+    $(".loginError").hide();
+    $(".summarizeError").hide();
 }
 
 function loggedInFeatures() {
@@ -26,121 +25,120 @@ function loggedOutFeatures() {
     $("#deleteAccountButton").hide();
 }
 
-function addButtonListeners() {
-    $("#signupButton").on('click', () => {
-        // show loading icon
-        $("#signupButton").toggleClass('is-loading');
-        // revert icon color on each button click
-        $(".fa-id-badge").css("color", "#009fff");
+function addButtonListeners() {
+    $("#signupButton").on('click', () => {
+        // show loading icon
+        $("#signupButton").toggleClass('is-loading');
+        // revert icon color on each button click
+        $(".fa-id-badge").css("color", "#009fff");
 
-        setTimeout(()=> {
-            newAccount();
-            // synchoronize removal of loading icon and newAccount
-            $("#signupButton").removeClass('is-loading');
-        }, 250);
+        setTimeout(() => {
+            newAccount();
+            // synchoronize removal of loading icon and newAccount
+            $("#signupButton").removeClass('is-loading');
+        }, 250);
 
-        hideElements();
-    });
-
-    $("#loginButton").on('click', () => {
-        $("#loginButton").toggleClass('is-loading');
-        $(".fa-id-badge, .fa-lock").css("color", "#009fff");
-
-        setTimeout(()=> {
-            postLogin();
-            $("#loginButton").removeClass('is-loading');
-        }, 250);
-
-        hideElements();
-    });
-
-    $("#logoutButton").on('click', () => {
-        $("#logoutButton").toggleClass('is-loading');
-
-        setTimeout(()=> {
-            logout();
-            $("#logoutButton").removeClass('is-loading');
-        }, 250);
-    });
-
-    $("#redirSignupButton").on('click', () => {
-        $("#redirSignupButton").toggleClass('is-loading');
-
-        setTimeout(()=> {
-            window.location.href = "signup.html";
-            $("#redirSignupButton").removeClass('is-loading');
-        }, 250);
-    });
-
-    $("#redirLoginButton").on('click', () => {
-        $("#redirLoginButton").toggleClass('is-loading');
-
-        setTimeout(()=> {
-            window.location.href = "login.html" ;
-            $("#redirLoginButton").removeClass('is-loading');
-        }, 250);
-    });
-
-    $("#redirGithubButton").on('click', () => {
-        $("#redirGithubButton").toggleClass('is-loading');
-
-        setTimeout(()=> {
-            window.location.href = "https://github.com/adamalston/Summarizer";
-            $("#redirGithubButton").removeClass('is-loading');
-        }, 250);
-    });
-
-    $("#summarizeButton").on('click', () => {
-        
-        $("#summarizeButton").toggleClass('is-loading');
-
-        setTimeout(()=> {
-            $("#summarizeButton").removeClass('is-loading');
-            // add an async method to get the summary
-            summarize();
-        }, 250);
-    });
-
-//     $("#saveSmmryButton").on('click', () => {
-//         saveSmmry();
-//     });
-
-    $("#deleteAccountButton").on('click', () => {
-        $("#deleteAccountButton").toggleClass('is-loading');
-
-        setTimeout(()=> {
-            deleteAccount();
-            $("#deleteAccountButton").removeClass('is-loading');
-        }, 250);
+        hideElements();
     });
 
-    $(".navbar-burger").on('click', () => {
-        // Toggle is-active class on both the navbar-burger and the navbar-menu
-        $(".navbar-burger").toggleClass("is-active");
-        $(".navbar-menu").toggleClass("is-active");
-    });
+    $("#loginButton").on('click', () => {
+        $("#loginButton").toggleClass('is-loading');
+        $(".fa-id-badge, .fa-lock").css("color", "#009fff");
+
+        setTimeout(() => {
+            postLogin();
+            $("#loginButton").removeClass('is-loading');
+        }, 250);
+
+        hideElements();
+    });
+
+    $("#logoutButton").on('click', () => {
+        $("#logoutButton").toggleClass('is-loading');
+
+        setTimeout(() => {
+            logout();
+            $("#logoutButton").removeClass('is-loading');
+        }, 250);
+    });
+
+    $("#redirSignupButton").on('click', () => {
+        $("#redirSignupButton").toggleClass('is-loading');
+
+        setTimeout(() => {
+            window.location.href = "signup.html";
+            $("#redirSignupButton").removeClass('is-loading');
+        }, 250);
+    });
+
+    $("#redirLoginButton").on('click', () => {
+        $("#redirLoginButton").toggleClass('is-loading');
+
+        setTimeout(() => {
+            window.location.href = "login.html";
+            $("#redirLoginButton").removeClass('is-loading');
+        }, 250);
+    });
+
+    $("#redirGithubButton").on('click', () => {
+        $("#redirGithubButton").toggleClass('is-loading');
+
+        setTimeout(() => {
+            window.location.href = "https://github.com/adamalston/Summarizer";
+            $("#redirGithubButton").removeClass('is-loading');
+        }, 250);
+    });
+
+    $("#summarizeButton").on('click', () => {
+
+        $("#summarizeButton").toggleClass('is-loading');
+
+        setTimeout(() => {
+            $("#summarizeButton").removeClass('is-loading');
+            summarize();
+        }, 250);
+    });
+
+    //     $("#saveSmmryButton").on('click', () => {
+    //         saveSmmry();
+    //     });
+
+    $("#deleteAccountButton").on('click', () => {
+        $("#deleteAccountButton").toggleClass('is-loading');
+
+        setTimeout(() => {
+            deleteAccount();
+            $("#deleteAccountButton").removeClass('is-loading');
+        }, 250);
+    });
+
+    $(".navbar-burger").on('click', () => {
+        // Toggle is-active class on both the navbar-burger and the navbar-menu
+        $(".navbar-burger").toggleClass("is-active");
+        $(".navbar-menu").toggleClass("is-active");
+    });
 }
 
-const accountRoot = axios.create({
-    baseURL: "http://localhost:3000/account"
+const accountRoot = axios.create({
+    baseURL: "http://localhost:3000/account"
 });
 
-async function newAccount() {
-    try {
-        const result = await accountRoot.post(`/create`,{
-        "name": $("#newName").val(),
-        "pass": $("#newPass").val(),
-        "data": {
-            "firstname": $("#firstname").val(),
-            "lastname": $("#lastname").val(),
-        }
-    });
-        window.location.replace("login.html");
-    } catch (error) {
-        $(".signupError").show();
-        // change icon color to red indicating error in field
-        $(".fa-id-badge").css("color", "red");
-    }
+async function newAccount() {
+    try {
+        const result = await accountRoot.post(`/create`, {
+            "name": $("#newName").val(),
+            "pass": $("#newPass").val(),
+            "data": {
+                "firstname": $("#firstname").val(),
+                "lastname": $("#lastname").val(),
+            }
+        });
+        window.location.replace("login.html");
+    } catch (error) {
+        $(".signupError").show();
+        // change icon color to red indicating error in field
+        $(".fa-id-badge").css("color", "red");
+    }
 }
 
 async function postLogin() {
@@ -174,14 +172,14 @@ async function deleteAccount() {
         let username = await checkStatus();
         console.log(username);
         if (username === undefined) {
-            throw "Username is undefined"
+            throw "Username is undefined";
         } else {
             const res = await accountRoot.delete(`/${username}`);
             localStorage.removeItem("jwt");
             window.location.replace("signup.html");
         }
     } catch (error) {
-        console.log("error");
+        console.log(error);
     }
 }
 
@@ -199,15 +197,15 @@ async function checkStatus() {
     }
 }
 
-function logout() {
-    if (localStorage.getItem("jwt") != null) {
-        localStorage.removeItem("jwt");
-        window.location.replace("login.html");
-    }
+function logout() {
+    if (localStorage.getItem("jwt") != null) {
+        localStorage.removeItem("jwt");
+        window.location.replace("login.html");
+    }
 }
 
-const smmryRoot = axios.create({
-    baseURL: "http://localhost:3000/smmry"
+const smmryRoot = axios.create({
+    baseURL: "http://localhost:3000/smmry"
 });
 
 async function summarize() {
@@ -217,50 +215,71 @@ async function summarize() {
         "url": url,
     });
     let id = res.data;
-    
-    let testerFun = async function(id) {
+
+    let testerFun = async function (id) {
         try {
-            //console.log("testerFun id: ", id);
-            const result = await smmryRoot.post(`/getID`, {
-                "id": id,
-            });	
-            console.log(result.data.data);
-            let smmryObj = result.data.data;
+            let smmryObj = await getSummaryObject(id);
             let title = smmryObj.data.sm_api_title;
             let body = smmryObj.data.sm_api_content;
             let url = smmryObj.url;
-
-            document.getElementById("title").innerHTML = `${title}`;
-            document.getElementById("content").innerHTML = `${body}`;
+            populateMain(smmryObj);
+            // document.getElementById("title").innerHTML = `${title}`;
+            // document.getElementById("content").innerHTML = `${body}`;
             console.log("success");
         } catch (error) {
             console.log("error");
-            setTimeout(testerFun, 1000, id);  
+            setTimeout(testerFun, 1000, id);
         }
     }
-    setTimeout(testerFun, 1000, id); 
+    setTimeout(testerFun, 1000, id);
 }
 
-// async function getSummaryObject(id) {
-//     return await smmryRoot.get(`id.${id}`)
+async function getSummaryObject(id) {
+    const result = await smmryRoot.post(`/getID`, {
+        "id": id,
+    });
+    let obj = result.data.data;
+    return obj;
+}
+
+function populateMain(object) {
+    let smmry = object;
+    let title = smmry.data.sm_api_title;
+    let content = smmry.data.sm_api_content;
+    let source = smmry.url;
+    let notes = '';
+
+    let smmryMarkup =
+    `
+    <div class="content" id="mainContent">
+        <h4 class="has-text-centered" id="title">${title}</h4>
+        <p id="content">${content}</p>
+        <p><a href="${source}" id="source">${source}</a></p>
+        <p>Notes: ${notes}<p>
+    </div>
+    `;
+    $("#mainContent").remove();
+    $("#mainCard").append($(smmryMarkup));
+}
+
+// async function populateSecondaries(store) {
+
 // }
 
 
 
 let blinker = document.getElementById('blink');
 if (blinker != null) {
-    setInterval(function() {
+    setInterval(function () {
         blinker.style.display = (blinker.style.display == 'none' ? '' : 'none');
     }, 1000);
 }
 
-
-$(document).ready(async() => {
-    hideElements();
-    addButtonListeners();
+$(document).ready(async () => {
+    hideElements();
+    addButtonListeners();
     let username = await checkStatus();
-    console.log(username);
-    
+
     if (username != undefined) {
         loggedInFeatures();
     } else {
@@ -275,6 +294,9 @@ $(document).ready(async() => {
 // separate view source from signup and login
 
 // --todo
+// create functions to populate main (done) and secondary SMMRY cards (not done)
+// when adding to userStore, add note functionality
+// fix the summarizeError
 
 // api data
 // {"sm_api_character_count":"546","sm_api_content_reduced":"91%","sm_api_title":"Keyboard class action lawsuit against Apple should succeed",
